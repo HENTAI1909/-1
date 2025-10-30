1fea06a615f659e7ad95d569ee1a0c1cf2d653c9
HENTAI1909 <113054255+HENTAI1909@users.noreply.github.com>
2025-10-30 22:30:10 +0200

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    Console.Write("Введите A: ");
+    int A = int.Parse(Console.ReadLine());
+    Console.Write("Введите B: ");
+    int B = int.Parse(Console.ReadLine());
+    int count = 0;
+    Console.WriteLine("Числа между A и B в порядке убывания:");
+        for (int i = B - 1; i > A; i--)
+    {
+        Console.WriteLine(i);
+        count++;
+    }
+    Console.WriteLine($"Количество чисел: {count}");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
